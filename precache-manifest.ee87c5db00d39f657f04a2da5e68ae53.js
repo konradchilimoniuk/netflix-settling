da3f9b75453c9b6c2b365adf2d5ceae22214da29
Konrad Chilimoniuk <chilimoniuk.konrad@gmail.com>
2020-04-30 02:02:03 +0200

    Updates

---
+++ precache-manifest.ee87c5db00d39f657f04a2da5e68ae53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "438278f1957e39fd198a9b4ec026e09a",
+    "url": "/netflix-settling/index.html"
+  },
+  {
+    "revision": "d3da1eff90877cf43a01",
+    "url": "/netflix-settling/static/css/main.a3f05ea9.chunk.css"
+  },
+  {
+    "revision": "27b254a6433a197e3286",
+    "url": "/netflix-settling/static/js/2.74205e17.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/netflix-settling/static/js/2.74205e17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3da1eff90877cf43a01",
+    "url": "/netflix-settling/static/js/main.85e17d1f.chunk.js"
+  },
+  {
+    "revision": "5955923e0ceb07d5b15f",
+    "url": "/netflix-settling/static/js/runtime-main.3e28b552.js"
+  }
+]);
